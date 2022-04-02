c5e21783c99d542f647983fbaf408a7bb7a0b593
manuvats <manuvats1990@gmail.com>
2022-04-02 21:47:22 +0530

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,11 +1,10 @@
-# load train and test files
+# load the train and test
 # train algo
-# save the metrics, parameters
-
+# save the metrices, params
 import os
-import pandas as pd
 import warnings
 import sys
+import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
@@ -15,6 +14,7 @@ import argparse
 import joblib
 import json
 
+
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
@@ -23,15 +23,15 @@ def eval_metrics(actual, pred):
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
-    test_data_path = config["split_data"]["test_path"] 
+    test_data_path = config["split_data"]["test_path"]
     train_data_path = config["split_data"]["train_path"]
     random_state = config["base"]["random_state"]
-    model_dir = ["model_dir"]
+    model_dir = config["model_dir"]
 
-    alpha = ["estimators"]["ElasticNet"]["alpha"]
-    l1_ratio = ["estimators"]["ElasticNet"]["l1_ratio"]
+    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
 
-    target = ["base"]["target_col"]
+    target = [config["base"]["target_col"]]
 
     train = pd.read_csv(train_data_path, sep=",")
     test = pd.read_csv(test_data_path, sep=",")
@@ -45,16 +45,48 @@ def train_and_evaluate(config_path):
     lr = ElasticNet(
         alpha=alpha, 
         l1_ratio=l1_ratio, 
-        random_state=random_state
-        )
+        random_state=random_state)
     lr.fit(train_x, train_y)
 
     predicted_qualities = lr.predict(test_x)
     
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+#####################################################
+    scores_file = config["reports"]["scores"]
+    params_file = config["reports"]["params"]
+
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+        }
+        json.dump(params, f, indent=4)
+#####################################################
+
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(lr, model_path)
+
+
+
 if __name__=="__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default = "params.yaml")
+    args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    train_and_evaluate(config_path = parsed_args.config)
+    train_and_evaluate(config_path=parsed_args.config)
