d3c0b40ad45905bda4a186d347ca6210f34eca2d
manuvats <manuvats1990@gmail.com>
2022-04-02 21:27:03 +0530

    stage 3 complete

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -10,7 +10,6 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-from urlib.parse import urlparse
 from get_data import read_params
 import argparse
 import joblib
